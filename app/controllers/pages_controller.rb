88c0b5a58b7c15e08a63634fc4ec61c6ab83ddf0
Jeff Cohen <jeff@starterleague.com>
2014-02-18 15:28:53 -0600

    Ready for class

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,2 +1,26 @@
   class PagesController < ApplicationController
+
+
+    def home
+      @username = params[:name]
+    end
+
+    def sports
+      @username = params[:name]
+    end
+
+    def food
+      @username = params[:name]
+    end
+
+    def places
+      @username = params[:name]
+    end
+
+    def login
+      @username = params[:name]
+
+      render 'home'
+    end
+
   end
